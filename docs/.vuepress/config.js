92913d39c6a41e6864d6c476fae9724c9c0578d9
chenyn22577 <chenyn22577@hundsun.com>
2020-05-28 19:36:36 +0800

    fix:调整层级目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,25 +5,19 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/icon/ico-pig.png' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
-  //base: '/vuepress-blog/', // 这是部署到github相关的配置 下面会讲
   markdown: {
     lineNumbers: true, // 代码块显示行号
-    // extractHeaders: [ 'h2', 'h3', 'h4','h5' ]
   },
-  // theme: 'reco',
   plugins: ['@vuepress/blog'] ,
   themeConfig: {
     // 博客配置
-    // type: 'blog',
-    authorAvatar: '/image/logo.jpg',
     sidebar: 'auto',//在所有页面中启用自动生成侧栏
     sidebarDepth: 2, // 将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
     lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
 	nav:[
-      { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
-      { text: '后端', link: '/dev/' }, // 内部链接 以docs为根目录
-      { text: '前端', link: '/front/' }, 
-      { text: '我的工程', link: '/mydocslist/' }, 
+      { text: '我的工程', link: '/00.mydocslist/' }, 
+      { text: '后端', link: '/01.dev/' }, // 内部链接 以docs为根目录
+      { text: '前端', link: '/02.front/' }, 
       // 下拉列表
       {
         text: 'GitHub',
@@ -33,25 +27,25 @@ module.exports = {
       }        
     ],
 	sidebar:{
-      '/dev/': [
+      '/01.dev/': [
         {
           title:'nginx基础入门',
           children:[
-            '/dev/nginx/nginxstudy'
+            '/01.dev/11.nginx/nginxstudy'
           ]
         },
         {
           title:'FastDFS安装和使用',
           children:[
-            '/dev/fastdfs/fastdfsStudy'
+            '/01.dev/10.fastdfs/fastdfsStudy'
           ]
         },
       ],
-      '/front/': [
+      '/02.front/': [
         {
           title:'玩转vuepress',
           children:[
-            '/front/markdown/manual'
+            '/02.front/10.markdown/manual'
           ]
         }
       ]
---
+++ docs/01.dev/README.md
@@ -0,0 +1,9 @@
+## 目录
+
+## 1、[nginx基础入门](/01.dev/11.nginx/nginxstudy)
+
+基础nginx搭建demo
+
+## 2、[FastDFS安装和使用](/01.dev/10.fastdfs/fastdfsStudy)
+
+介绍fastdfs原理，提供fastdfs安装和使用的指导
